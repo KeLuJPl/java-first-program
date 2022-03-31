5f2bac4c5e131edd62f16a13f8e5cb9b9d0c736d
KeLuJPl <51032859+KeLuJPl@users.noreply.github.com>
2022-03-31 21:08:47 +0200

    Task 2.

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,29 @@
 package com.h2;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
     }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum = sum + number;
+        }
+
+        return sum;
+    }
+
+
 }
