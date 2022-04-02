09c211bd9cf9a44ad3bd98891fe3867f5f093d76
KeLuJPl <51032859+KeLuJPl@users.noreply.github.com>
2022-04-02 22:42:56 +0200

    Task 4.8

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,17 +1,31 @@
 package com.h2;
 
+import java.text.DecimalFormat;
+
 public class MortgageCalculator {
 
-    private long loanAmount;
-    private int termInYears;
-    private float annualRate;
+    public static void main(String[] args){
+        long loanAmount = Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+
+        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
+        calculator.calculateMonthlyPayment();
+
+        System.out.println(calculator.toString());
+    }
+
+
+    private final long loanAmount;
+    private final int termInYears;
+    private final float annualRate;
     private double monthlyPayment;
 
 
     public MortgageCalculator(long loanAmount, int termInYears, float annualRate){
         this.loanAmount = loanAmount;
         this.termInYears = termInYears;
-        this. annualRate = annualRate;
+        this.annualRate = annualRate;
     }
 
     private int getNumberOfPayments(){
@@ -24,6 +38,20 @@ public class MortgageCalculator {
     }
 
     public void calculateMonthlyPayment(){
+        long P = loanAmount;
+        float r = getMonthlyInterestRate();
+        int n = getNumberOfPayments();
 
+        double M;
+        M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
+
+        this.monthlyPayment = M;
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
     }
 }
+
